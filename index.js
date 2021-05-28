c9d5bb7052bde213dcdebbdbc76650e4e65424e9
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-05-28 09:58:36 -0500

    set up Timer class and add input/start/stop button

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hi");
+// Create a class
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    // Listen for click event on the start button
+    this.startButton.addEventListener("click", this.start);
+    // Listen for click event on the start button
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+  // Start method for Timer 
+  start() {
+    console.log("Start Timer");
+  }
+  // Pause method for Timer
+  pause() {
+    console.log("Pause Timer");
+  }
+}
+
+// Assigning the contructor arguments to DOM elements
+const durationInput = document.querySelector("#duration");
+const startButton= document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+// ☝🏼 Create new instance of timer and pass in elements from above 
+const timer = new Timer(durationInput, startButton, pauseButton)
